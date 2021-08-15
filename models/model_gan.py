7b0c18e6c89250042910b721f42a0c9db3bc50b3
birdy666 <allenchang1227@gmail.com>
2021-08-16 00:16:42 +0200

    20210816_02 modified discriminator

--- models/model_gan.py
+++ models/model_gan.py
@@ -46,26 +46,32 @@ class Discriminator(nn.Module):
         self.position_enc = PositionalEncoding(cfg.D_WORD_VEC)
         self.dropout = nn.Dropout(p=cfg.DROPOUT_D)
         self.layer_norm = nn.LayerNorm(cfg.D_WORD_VEC, eps=1e-6)
-        self.encoder_stack = nn.ModuleList([EncoderLayer(d_model=cfg.D_MODEL_LIST_D[i], 
-                                                        d_inner=cfg.D_MODEL_LIST_D[i] * cfg.D_INNER_SCALE_D, 
-                                                        n_head=cfg.N_HEAD_LIST_D[i], 
-                                                        d_k=cfg.D_K_LIST_D[i], 
-                                                        d_v=cfg.D_V_LIST_D[i], 
-                                                        dropout=cfg.DROPOUT_D)
-                                                        for i in range(cfg.N_LAYERS_D)])  
+        self.encoder_stack = nn.ModuleList([EncoderLayer(d_model=300, 
+                                                        d_inner=4 * 300, 
+                                                        n_head=4, 
+                                                        d_k=32, 
+                                                        d_v=32, 
+                                                        dropout=0.1)
+                                                        for i in range(5)])  
         self.fc_stack = nn.ModuleList([nn.Linear(cfg.D_MODEL_LIST_D[i], cfg.D_MODEL_LIST_D[i+1], bias=False) for i in range(cfg.N_LAYERS_D)])  
         self.fc = nn.Linear(24, 1, bias=False)
+        self.fcs = nn.Sequential(nn.Linear(300, 128, bias=False),
+                                nn.Linear(128, 64, bias=False),
+                                nn.Linear(64, 32, bias=False),
+                                nn.Linear(32, 16, bias=False),
+                                nn.Linear(16, 8, bias=False),
+                                nn.Linear(8, 4, bias=False),
+                                nn.Linear(4, 1, bias=False))
 
     def forward(self, so3, sentence_vec, src_mask=None):
         # -- Forward
         """圖片上的normalize還有dropout在PositionwiseFeedForward"""
         if self.cfg.SCALE_EMB_D:
             sentence_vec = sentence_vec * self.cfg.D_MODEL_LIST_D[0] ** 0.5
-        else:
-            sentence_vec = sentence_vec
-        sentence_vec = self.dropout(self.position_enc(sentence_vec))
+        
+        """sentence_vec = self.dropout(self.position_enc(sentence_vec))
         # 128 x 24 x 150
-        sentence_vec  = self.layer_norm(sentence_vec)
+        sentence_vec  = self.layer_norm(sentence_vec)"""
         
         
         """# 128 x 24 x 150 直接用fasttext中的降為降成150
@@ -73,19 +79,17 @@ class Discriminator(nn.Module):
         # 128 x 24 x 300
         """TODO 這裡50是寫死的"""
         enc_output = torch.cat((sentence_vec, so3.repeat(1,1,50)), -1)
-        for i in range(self.cfg.N_LAYERS_D):
+        for enc_layer in self.encoder_stack:
             """
             enc_output: (batch, text_len, word_emb_len)
             src_mask: (batch, text_len) ==> unsqueeze(-2) ==> (batch, 1, text_len)
             """
-            enc_output = self.encoder_stack[i](enc_output)
-            enc_output = self.fc_stack[i](enc_output)
+            enc_output = enc_layer(enc_output)
         """
         # (128, 24, 3+3)
         so3_enc_output = torch.cat((so3, enc_output), -1)
         output, _ =  self.encoder_last(so3_enc_output, slf_attn_mask=None)"""
-        # 128 x 24 x 1
-        output = self.fc(enc_output.view(-1, self.cfg.JOINT_NUM))
+        output = self.fcs(enc_output).view(128,-1)
         #output = F.sigmoid(output)
         return output
 
